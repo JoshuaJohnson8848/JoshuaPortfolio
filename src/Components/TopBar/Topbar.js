6e76c703a0aeeb53d8d38598d3476c9205de4ff1
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-01-27 16:31:39 +0530

    Added TopBar Structure

--- src/Components/TopBar/Topbar.js
+++ src/Components/TopBar/Topbar.js
@@ -1,7 +1,70 @@
 import React from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../TopBar/TopBar.css';
+import { useNavigate } from 'react-router-dom';
 
-function Topbar() {
-  return <div>Topbar</div>;
+function TopBar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Navbar className="color_nav">
+        <Container>
+          <Nav className="m-auto">
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Navbar.Brand>
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/about');
+              }}
+            >
+              About
+            </Navbar.Brand>
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/skills');
+              }}
+            >
+              Skills
+            </Navbar.Brand>
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/projects');
+              }}
+            >
+              Projects
+            </Navbar.Brand>
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/education');
+              }}
+            >
+              Education
+            </Navbar.Brand>
+
+            <Navbar.Brand
+              className="Navtext_color"
+              onClick={() => {
+                navigate('/contact');
+              }}
+            >
+              Contact
+            </Navbar.Brand>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Topbar;
+export default TopBar;
