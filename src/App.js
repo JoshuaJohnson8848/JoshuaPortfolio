b18c732f88cc53a84357d86725f0fde423173b49
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-01-27 16:44:19 +0530

    This is the Master Branch With TopBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import TopBar from './Components/TopBar/TopBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <TopBar />
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
