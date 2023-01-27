f890d965788fd4444b9543366b4d8b76f76084b6
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-01-27 16:52:38 +0530

    Finished Functionalities of Banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import TopBar from './components/TopBar/TopBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <TopBar />
+        <Banner />
         <Routes></Routes>
       </BrowserRouter>
     </div>
